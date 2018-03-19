3cc4a9388ec809d3aa66e45a2e4eec17874a4ef8
Alen Šimec <alen@eburza.hr>
2018-03-19 12:44:34 +0100

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,22 @@
+<?php 
+	$operation = $_GET["op"];
+	$xmlFile = simplexml_load_file("AccountInfo.xml");
+	$xmlLocalCopy = new SimpleXMLElement($xmlFile->asXML());
+		
+	if($operation == "sendInfo")
+	{
+		$username = $_GET["username"];
+		$password = $_GET["password"];
+		$newAcct = $xmlLocalCopy->addChild("Accounts");	
+		$newAcct->addChild("username", $username);
+		$newAcct->addChild("password", $password);
+			
+		$dom = new DOMDocument('1.0');
+		$dom->preserveWhiteSpace = false;
+		$dom->formatOutput = true;
+		$dom->loadXML($xmlLocalCopy->asXML());
+		$dom->save("AccountInfo.xml");
+		
+		echo "Korisnik kreiran.%login.html";
+	}	
+?>
